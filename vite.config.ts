ea02c27d6f40260cf83dffc0f8787d9175f78c47
Mohammed Afzal <108460168+Afzal-exe@users.noreply.github.com>
2025-02-13 22:00:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    host: true,  // Allow external access
+    port: 8080,  // Use Azure's default port
   },
 });
+
