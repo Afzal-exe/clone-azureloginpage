520975e1c7eb3f401eaf973bdb36118e704d738e
Mohammed Afzal <108460168+Afzal-exe@users.noreply.github.com>
2025-02-13 22:25:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    host: true,  // Allow external access
-    port: 8080,  // Use Azure's default port
+    host: "0.0.0.0", // Allows external access
+    port: process.env.PORT || 5173, // Uses Azure's assigned port or 5173 for local
   },
 });
-
